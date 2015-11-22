3459f9b31ded2390a44b777744ae0644884f6e41
Viktor <vima00@seznam.cz>
2015-11-22 03:53:35 +0100

    getYTComments

--- youtube.js
+++ youtube.js
@@ -1,24 +1,32 @@
-$(function(){
-  var videoID = 'wtLJPvx7-ys';
-
-
-  var videoURL = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=' + videoID + '&key=AIzaSyDKOmEoe8tm50XoIFSD6r9TQCoJCOlWPx0'
-  $.getJSON(videoURL, function(data) {
-    if(data && data.items){
-      console.log(data.items[0].contentDetails.duration);
-    }
-  });
-
-
+function getYTComments(videoID, callback){
   var commentsURL = 'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=100&videoId=' + videoID + '&key=AIzaSyDKOmEoe8tm50XoIFSD6r9TQCoJCOlWPx0'
   var comments = [];
   $.getJSON(commentsURL, function(data) {
+    var a_regex = /(.*)<a\s+(?:[^>]*?\s+)?href="([^"]*t=([^"]*))">([^<]*)<\/a>(.*)/;
+    var time_regex = /(\d?\dh)?(\d?\dm)?(\d?\ds)/
     if(data && data.items){
       for(var i = 0; i < data.items.length; i++){
         var text = data.items[i].snippet.topLevelComment.snippet.textDisplay;
-        if(text)
-        console.log(data.items[i].snippet.topLevelComment.snippet.textDisplay);
+        var author = data.items[i].snippet.topLevelComment.snippet.authorDisplayName;
+        var match = text.match(a_regex);
+        if(match !== null){
+          text = match[0].replace(/<(?:.|\n)*?>/gm, '');
+          var time = match[3].match(time_regex);
+          var seconds = parseInt(time[1] || 0) * 3600 + parseInt(time[2] || 0) * 60 + parseInt(time[3] || 0);
+          var comment = {
+            time: seconds,
+            text: text,
+            author: author
+          }
+          comments.push(comment);
+        }
       }
     }
+  }).done(function(){
+    callback(comments);
   });
+}
+
+$(function(){
+  getYTComments("wtLJPvx7-ys", function(data){ console.log(data) });
 });
