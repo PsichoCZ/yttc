f68088de90c343a3f3cc808d4fb0eee434944d51
prespic <prespic@gmail.com>
2015-11-22 01:40:58 +0100

    fwsf

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 
 var progress= $('.ytp-progress-list');
 console.log(progress);
-var mark = $("<div class='mark'>test</div>");
+var mark = $("<a class='mark' href='http://www.youtube.com/watch?v=RVr0lbKZg5Q&t=45m05s'\
+					  class='yt-uix-sessionlink'  data-sessionlink='ei=oQ5RVpWiG4aiWuf0mtgM' >\
+				<div class='title'>test</div>\
+				<div class='button'></div>\
+			<a>");
 progress.append(mark);
  
  
